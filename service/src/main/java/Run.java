f78a4d351f562fa5860ce980d3f986886e10a7bb
Catherine <sunny4evil@gmail.com>
2023-03-25 15:36:32 +0300

    reload idea

--- service/src/main/java/Run.java
+++ service/src/main/java/Run.java
@@ -3,7 +3,7 @@ import dao.Dao;
 public class Run {
     public static void main(String[] args) {
         System.out.println("Iyi misin?");
-        Dao dao = new Dao();
-        System.out.println(dao.find());
+//        Dao dao = new Dao();
+//        System.out.println(dao.find());
     }
 }
