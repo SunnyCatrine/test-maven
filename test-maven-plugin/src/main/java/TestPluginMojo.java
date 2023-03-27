6def6db02dd1e54771d59be7fcf8bacb9cba387c
Catherine <sunny4evil@gmail.com>
2023-03-27 12:10:30 +0300

    add parametr message to goal TestPlugin

--- test-maven-plugin/src/main/java/TestPluginMojo.java
+++ test-maven-plugin/src/main/java/TestPluginMojo.java
@@ -2,12 +2,16 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name="TestPlugin")
 public class TestPluginMojo extends AbstractMojo {
 
+    @Parameter(defaultValue = "Test message", property = "message")
+    private String message;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Test message");
+        getLog().info(message);
     }
 }
